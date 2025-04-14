7a7d15f33efff4929e1783ed49182b45af0f588b
May <abeillee@bk.ru>
2025-04-14 14:59:37 +0300

    feat: add Container class

---
+++ fun-chat/src/app/view/container/container.ts
@@ -0,0 +1,14 @@
+import { View } from '../view';
+import { Options } from '../../utils/types';
+
+export class ContainerView extends View {
+    constructor(className: string[], parent?: HTMLElement) {
+        const options: Options = {
+            tagName: 'div',
+            classes: ['container', ...className],
+            parent: parent,
+        };
+
+        super(options);
+    }
+}
