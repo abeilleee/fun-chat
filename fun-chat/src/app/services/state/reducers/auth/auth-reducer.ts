2b6347bf41e8fe81117a5955fe812a4a86b35050
May <abeillee@bk.ru>
2025-04-22 21:16:46 +0300

    refactor: refactor checkErrors

--- fun-chat/src/app/services/state/reducers/auth/auth-reducer.ts
+++ fun-chat/src/app/services/state/reducers/auth/auth-reducer.ts
@@ -1,12 +1,18 @@
 import { Modal } from '../../../../components/modal/modal';
 import { APP_ERROR } from '../../../server-api/constants';
+import { ERRORS } from '../../types';
 
 export function checkErrors(data: string): void {
     const { id, type, payload } = JSON.parse(data);
 
     if (type === APP_ERROR.ERROR) {
         const error: string = payload.error;
-        const modal = new Modal();
-        modal.showModal(error);
+
+        if (error === ERRORS.DOUBLE_LOGIN || error === ERRORS.DOUBLE_USER) {
+            const modal = new Modal();
+            modal.showModal(error);
+        } else {
+            console.error(error);
+        }
     }
 }
--- fun-chat/src/app/services/state/types.ts
+++ fun-chat/src/app/services/state/types.ts
@@ -8,3 +8,9 @@ export type UserUnreadMessages = {
     username: string;
     unreadMessages: number;
 };
+
+export enum ERRORS {
+    NOT_RECIPIENT = 'user not recipient cannot be executed',
+    DOUBLE_LOGIN = 'a user with this login is already authorized',
+    DOUBLE_USER = 'another user is already authorized in this connection',
+}
