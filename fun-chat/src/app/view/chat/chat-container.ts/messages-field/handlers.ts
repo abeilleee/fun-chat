88e98d82121e92c7ea89594f24597e0aa8510aea
May <abeillee@bk.ru>
2025-04-22 20:52:47 +0300

    fix: fix handlerReadingMessages

--- fun-chat/src/app/view/chat/chat-container.ts/messages-field/handlers.ts
+++ fun-chat/src/app/view/chat/chat-container.ts/messages-field/handlers.ts
@@ -4,6 +4,7 @@ import type { ClientApi } from '../../../../services/server-api/client-api';
 import type { Dialog, Message } from '../../../../services/server-api/types/chat';
 import { changeDialogState, dialogState } from '../../../../services/state/reducers/dialog/dialog-reducer';
 import { selectedUser } from '../../../../services/state/reducers/users/user-states-reducer';
+import { getCurrentUsername } from '../../../../services/storage/storage';
 
 export function dialogWrapperHandler(clientApi: ClientApi): void {
     changeMessagesStatus(clientApi);
@@ -30,7 +31,9 @@ export function handlerReadingMessages(clientApi: ClientApi): void {
     const state = dialogState;
     const targetDialog = state.find((dialog: Dialog) => dialog.login === selectedUser.username);
     targetDialog?.messages.forEach((message: Message) => {
-        if (message.status && !message.status.isReaded) clientApi.msgReadStatusChange(message);
+        const currentUser = getCurrentUsername();
+        if (message.to === currentUser && message.status && !message.status.isReaded)
+            clientApi.msgReadStatusChange(message);
     });
     changeDialogState(state);
 }
